a370283ee7af37f18b246f3cf21db9c66a73be33
jeff <jk3@cs.cmu.edu>
2025-04-07 11:42:59 -0400

    InputTransform is now parameterizable by input dimension. Will now output a transform matrix in either 3x3 or 64x64 or dimxdim.

--- point_blocks.py
+++ point_blocks.py
@@ -5,12 +5,13 @@ import torch.nn.functional as F
 
 
 class InputTransform(nn.Module):
-    def __init__(self, args=None):
+    def __init__(self, args=None, dim=3):
         super(InputTransform, self).__init__()
         N = args.n_points
         self.device = args.device
+        self.dim = dim
         self.block1 = nn.Sequential(
-            nn.Conv2d(3,64,(1,1),1), # don't need any padding probably because it's a 1x1 kernel
+            nn.Conv2d(dim,64,(1,1),1), # don't need any padding probably because it's a 1x1 kernel
             nn.ReLU(),
             nn.BatchNorm2d(64),
         )
@@ -40,7 +41,7 @@ class InputTransform(nn.Module):
                 nn.ReLU(),
                 nn.BatchNorm1d(256)
         )
-        self.to_transform = nn.Linear(256, 9)
+        self.to_transform = nn.Linear(256, dim**2)
         def init(m):
             if isinstance(m, nn.Linear):
                 nn.init.constant_(m.weight, 0.)
@@ -48,17 +49,18 @@ class InputTransform(nn.Module):
         self.to_transform.apply(init)
 
 
-    def forward(self, X): # X is shape B,N,3
+    def forward(self, X): # X is shape B,N,dim
         B,N,*_ = X.shape
         X = X.unsqueeze(-1)
-        X = X.permute((0,2,1,3)) # shape is B,3,N,1
+        X = X.permute((0,2,1,3)) # shape is B,dim,N,1
         X = self.pointwise_mlps(X)
         X = self.maxpool2d(X) # collapses (B,1024,N,1) to (B,1024,1) probably
         X = X.view(B,-1)
         X = self.fc512(X)
         X = self.fc256(X)
-        X = self.to_transform(X) + torch.tensor([1., 0., 0., 0., 1., 0., 0., 0., 1.], device=self.device)
-        return X.view(B,3,3)
+        iden = torch.eye(self.dim, device=self.device).flatten()
+        X = self.to_transform(X) + iden
+        return X.view(B,self.dim,self.dim)
 
 class PerPointMLP(nn.Module):
     def __init__(self, in_dim, out_dims, args=None):
@@ -69,6 +71,7 @@ class PerPointMLP(nn.Module):
             mlps.append(nn.Conv2d(last_dim, d, (1,1),1)),
             mlps.append(nn.ReLU()),
             mlps.append(nn.BatchNorm2d(d))
+            last_dim = d
         self.perpoint_mlps = nn.Sequential(*mlps)
 
     def forward(self, X):
