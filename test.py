a803c1dc2f7ceb5779bf68c504db02e7a06e333b
jeff <jk3@cs.cmu.edu>
2025-04-07 11:44:21 -0400

    Testing that at least the shapes are outputted correctly.

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--n_points", type=int, default=1024, help="The number of points in each point cloud")
 parser.add_argument("--device", type=str, default="cuda")
 args = parser.parse_args()
-a = InputTransform(args).to("cuda")
+a = InputTransform(args, dim=64).to("cuda")
 input = torch.randn((100,1024,3), device="cuda")
-print(a(input))
+
+from models import cls_model
+
+m = cls_model(args).to("cuda")
+a = m(input, with_smax=True)
+print(a.shape)
